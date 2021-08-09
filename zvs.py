66765958011047c3a53caea7a277bbc66e70cb68
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2021-08-09 12:06:29 +0800

    somewhat usable

---
+++ zvs.py
@@ -0,0 +1,115 @@
+import vapoursynth as vs
+from vapoursynth import core
+import havsfunc as haf
+import xvs
+import mvsfunc as mvf
+
+def pqdenoise(src,sigma=[1,1,1],lumaonly=False,block_step=7,radius=1,finalest=False,bm3dtyp='cpu',mdegrain=True,pel=1,blksize=16,chromamv=True,thsad=100,thsadc=None,thscd1=400,thscd2=130,nl=100,contrasharp=1,show='output'):
+    if lumaonly:
+        chromamv=False
+        chromaclip=src
+        src=xvs.getY(src)
+
+    src=src.fmtc.bitdepth(bits=16)
+    denoised=sdr=core.resize.Bicubic(src,transfer_in=16,transfer=1,nominal_luminance=nl)
+    if mdegrain:
+        denoised=zmde(denoised,tr=2,thsad=thsad,thsadc=thsadc,blksize=blksize,pel=pel,thscd1=thscd1,thscd2=thscd2,chromamv=chromamv)
+        if show=='mde':
+            return denoised
+
+        if contrasharp>=2 or (contrasharp==1 and bm3dtyp=='no'):
+            denoised=haf.ContraSharpening(denoised,sdr)
+        if show=='mdecs':
+            return denoised
+    
+    if not bm3dtyp=='no':
+        if bm3dtyp=='cpu':
+            BM3D=core.bm3dcpu.BM3D
+        elif bm3dtyp=='cuda':
+            BM3D=core.bm3dcuda.BM3D
+        elif bm3dtyp=='cuda_rtc':
+            BM3D=core.bm3dcuda_rtc.BM3D
+
+        bdenoised=BM3D(denoised.fmtc.bitdepth(bits=32),sigma=sigma,radius=radius,block_step=block_step)
+        if not radius==0:
+            bdenoised=core.bm3d.VAggregate(bdenoised,radius,1)
+
+        if finalest:
+            bdenoised=BM3D(denoised.fmtc.bitdepth(bits=32),ref=bdenoised,sigma=sigma,radius=radius,block_step=max(block_step-1,1))
+            if not radius==0:
+                bdenoised=core.bm3d.VAggregate(bdenoised,radius,1)
+
+        denoised=bdenoised.fmtc.bitdepth(bits=16)
+        if show=='bm3d':
+            return denoised
+
+    if contrasharp>=1:
+        denoised=haf.ContraSharpening(denoised,sdr)
+    if show=='bm3dcs':
+        return denoised
+
+    denoised,sdr=[core.resize.Bicubic(i,transfer_in=1,transfer=16,nominal_luminance=nl) for i in (denoised,sdr)]
+    
+    output=core.std.Expr([src,sdr,denoised],'x y - z +')
+    if lumaonly:
+        output=core.std.ShufflePlanes([output,chromaclip],[0,1,2],vs.YUV)
+
+    return output
+
+
+def zmde(src,tr=2,thsad=100,thsadc=None,blksize=16,pel=1,chromamv=True,sharp=2,rfilter=4,truemotion=False,thscd1=400,thscd2=130):
+    if thsadc==None:
+        thsadc=thsad
+    last=src
+    if not chromamv:
+        pref=core.resize.Bicubic(last,range_in_s='limited',range_s='full')
+    else:
+        pref=last
+    
+    sup=core.mv.Super(pref,hpad=blksize,vpad=blksize,sharp=sharp,rfilter=rfilter,pel=pel)
+    sup2=core.mv.Super(last,hpad=blksize,vpad=blksize,sharp=sharp,levels=1,pel=pel)
+
+    mvfw=core.mv.Analyse(sup,isb=False,blksize=blksize,overlap=blksize//2,truemotion=truemotion,chroma=chromamv)
+    mvbw=core.mv.Analyse(sup,isb=True,blksize=blksize,overlap=blksize//2,truemotion=truemotion,chroma=chromamv)
+    if tr>=2:
+        mvfw2=core.mv.Analyse(sup,isb=False,delta=2,blksize=blksize,overlap=blksize//2,truemotion=truemotion,chroma=chromamv)
+        mvbw2=core.mv.Analyse(sup,isb=True,delta=2,blksize=blksize,overlap=blksize//2,truemotion=truemotion,chroma=chromamv)
+    elif tr>=3:
+        mvfw2=core.mv.Analyse(sup,isb=False,delta=3,blksize=blksize,overlap=blksize//2,truemotion=truemotion,chroma=chromamv)
+        mvbw2=core.mv.Analyse(sup,isb=True,delta=3,blksize=blksize,overlap=blksize//2,truemotion=truemotion,chroma=chromamv)
+    
+    if tr==1:
+        last=core.mv.Degrain(last,sup2,mvbw,mvfw,thsad=thsad,thsadc=thsadc,thscd1=thscd1,thscd2=thscd2)
+    elif tr==2:
+        last=core.mv.Degrain2(last,sup2,mvbw,mvfw,mvbw2,mvfw2,thsad=thsad,thsadc=thsadc,thscd1=thscd1,thscd2=thscd2)
+    elif tr>=3:
+        last=core.mv.Degrain3(last,sup2,mvbw,mvfw,mvbw2,mvfw2,mvbw3,mvfw3,thsad=thsad,thsadc=thsadc,thscd1=thscd1,thscd2=thscd2)
+    
+    return last
+
+
+def xdbcas(src,r=[8,15],y=[32,24],cb=[16,10],cr=[16,10],gy=[0,0],gc=[0,0],neo=False,casstr=0.7):
+    last=db=src.fmtc.bitdepth(bits=16)
+    r,y,cb,cr,gy,gc=[list(i) if isinstance(i,int) else i for i in (r,y,cb,cr,gy,gc)]
+    if neo:
+        f3k=core.neo_f3kdb.Deband
+    else:
+        f3k=core.f3kdb.Deband
+    
+    l1,l2,l3,l4,l5,l6=[len(i) for i in (r,y,cb,cr,gy,gc)]
+    if l1==l2==l3==l4==l5==l6:
+        passes=l6
+    else:
+        passes=min(l1,l2,l3,l4,l5,l6)
+
+    for i in range(passes):
+        db=f3k(db,r[i],y[i],cb[i],cr[i],gy[i],gc[i],output_depth=16)
+    db=mvf.LimitFilter(db,last,thr=0.1,thrc=0.05,elast=20,planes=[0,1,2])
+    dbmask=xvs.mwdbmask(last)
+    db=core.std.MaskedMerge(db,last,dbmask)
+
+    cas=core.cas.CAS(last,0.7,planes=[0,1,2])
+    cas=mvf.LimitFilter(cas,last,thr=0.3,thrc=0.15,brighten_thr=0.15,elast=4,planes=[0,1,2])
+    last=core.std.Expr([cas,last,db],'x y - z +')
+
+    return last
