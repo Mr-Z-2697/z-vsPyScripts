6b80777e6f506c8d1e66be7b01f80fe3de69d6ce
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2023-06-01 18:20:49 +0800

    Add files via upload

--- vsenv.py
+++ vsenv.py
@@ -1,7 +1,8 @@
 import vapoursynth as vs
-core = vs.core
-import xvs,zvs
-from zvs import nnrs
+core=vs.core
+core.num_threads/=2
+import xvs,zvs #love
+from zvs import nnrs #so i can use my default
 import mvsfunc as mvf
 import havsfunc as haf
 import muvsfunc as muf
