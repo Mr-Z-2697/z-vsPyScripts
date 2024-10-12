8212ee65eef5adaeca388997ec2889eaf0b07eec
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2024-10-12 08:08:32 +0800

    joke01: lrnoise: add arg for custom noise filter

--- joke01.py
+++ joke01.py
@@ -3,13 +3,16 @@ core=vs.core
 import zvs
 __all__=['lrnoise']
 
-def lrnoise(src,lr=(1280,720),gy=50,gc=0,hc=0,vc=0,con=0,seed=1,opt=0,a1=20,adg=False,mdg=False,azmdg={},cdif=False):
+def lrnoise(src,lr=(1280,720),gy=50,gc=0,hc=0,vc=0,con=0,seed=1,opt=0,a1=20,adg=False,mdg=False,azmdg={},cdif=False,fnoise=None):
     src=src.fmtc.bitdepth(bits=16)
     za={'thsad':1000,'truemotion':True}
     za.update(azmdg)
     last=src
     lr=last.fmtc.resample(lr[0],lr[1])
-    lrn=core.grain.Add(lr,var=gy,uvar=gc,hcorr=hc,vcorr=vc,constant=con,seed=seed,opt=opt)
+    if callable(fnoise):
+        lrn=fnoise(lr)
+    else:
+        lrn=core.grain.Add(lr,var=gy,uvar=gc,hcorr=hc,vcorr=vc,constant=con,seed=seed,opt=opt)
     if adg:
         lr=lr.std.PlaneStats()
         lrm=lr.adg.Mask(adg)
