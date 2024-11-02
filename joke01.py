3a1ca933dd21618ad3d1d01e8ffb2474272e46be
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2024-11-02 12:36:50 +0800

    debit: de-bit

--- joke01.py
+++ joke01.py
@@ -1,7 +1,7 @@
 import vapoursynth as vs
 core=vs.core
 import zvs
-__all__=['lrnoise']
+__all__=['lrnoise','debit']
 
 def lrnoise(src,lr=(1280,720),gy=50,gc=0,hc=0,vc=0,con=0,seed=1,opt=0,a1=20,adg=False,mdg=False,azmdg={},cdif=False,fnoise=None):
     src=src.fmtc.bitdepth(bits=16)
@@ -28,4 +28,29 @@ def lrnoise(src,lr=(1280,720),gy=50,gc=0,hc=0,vc=0,con=0,seed=1,opt=0,a1=20,adg=
         last=zvs.cdif(last,ndhr,1)
     else:
         last=core.std.MergeDiff(last,ndhr)
+    return last
+
+
+def debit(src,depth=1,dither=0,fulls=False,fulld=False):
+    if src.format.sample_type==vs.FLOAT:
+        raise NotImplementedError("panik")
+    if src.format.bits_per_sample < 16:
+        src=core.std.Expr(src,'x {} *'.format(2**(16-src.format.bits_per_sample)),src.format.replace(bits_per_sample=16))
+    if isinstance(dither,int):
+        rehtid=lambda x:x.fmtc.bitdepth(bits=8,dmode=dither)
+    elif isinstance(dither,str):
+        rehtid=lambda x:x.resize.Point(format=x.format.replace(bits_per_sample=8))
+    elif callable(dither):
+        rehtid=dither
+    scaling=255/(2**depth-1)
+    last=src
+    if not fulls:
+        last=zvs.setrange(last,'rm')
+        last=last.resize.Point(range_in_s='limited',range_s='full')
+    last=last.std.Expr(f'x {scaling} /')
+    last=rehtid(last)
+    last=last.std.Expr(f'x {scaling} *')
+    if not fulld:
+        last=zvs.setrange(last,'rm')
+        last=last.resize.Point(range_in_s='full',range_s='limited')
     return last
